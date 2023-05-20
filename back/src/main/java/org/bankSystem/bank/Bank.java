36641a474784cfef8d1b0a9e7179ed0d33f798b1
elena.ermakova <alena.ermakova2004@gmail.com>
2023-05-20 11:55:47 +0300

    fix: checkstyle

--- back/src/main/java/org/bankSystem/bank/Bank.java
+++ back/src/main/java/org/bankSystem/bank/Bank.java
@@ -3,7 +3,6 @@ package org.bankSystem.bank;
 /** Not abstract bank */
 public class Bank extends AbstractBank {
 
-
     public Bank(final String bankName, final String registrationID, final int creditLimit,
                 final double fee) {
         super(bankName, registrationID, creditLimit, fee);
